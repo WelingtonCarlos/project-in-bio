2e148cf603fd2b18c84297fa4999f53ac97cb8b9
Welington Filho <wcfilho98@hotmail.com>
2025-01-20 23:21:53 -0300

    feat: página de upgrade de plano concluída. todo: implementação das configurações com providers externos

--- app/(pages)/[profileId]/page.tsx
+++ app/(pages)/[profileId]/page.tsx
@@ -2,6 +2,7 @@ import ProjectCard from "@/app/components/common/project-card";
 import TotalVisits from "@/app/components/common/total-visits";
 import UserCard from "@/app/components/common/user-card";
 import { Plus } from "lucide-react";
+import Link from "next/link";
 export default async function ProfilePage({
   params,
 }: {
@@ -12,9 +13,11 @@ export default async function ProfilePage({
     <div className="flex space-y-8">
       <div className="fixed top-0 left-0 w-full flex justify-center items-center gap-1 py-4 bg-background-tertiary">
         <span>Você está usando a versão trial.</span>
-        <button className="text-accent-green font-bold">
-          Faça o upgrade agora!
-        </button>
+        <Link href={`/${profileId}/upgrade`}>
+          <button className="text-accent-green font-bold">
+            Faça o upgrade agora!
+          </button>
+        </Link>
       </div>
       <div className="w-1/2 flex justify-center">
         <UserCard />
